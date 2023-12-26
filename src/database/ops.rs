79547a062115fa1bdcee062664eca5b221597c24
Kirill Baranov <kirill.baranov@gmail.com>
2023-12-26 01:35:59 +0100

    implement exists_by_name ops method

--- src/database/ops.rs
+++ src/database/ops.rs
@@ -1,5 +1,5 @@
-use super::query::{Order, Query};
-use super::structs::{FieldNames, FromRow, InsertValues, TableName};
+use super::query::{Order, Query, WhereExprOperator};
+use super::structs::{FieldNames, FromRow, InsertValues, Named, TableName};
 use rusqlite::{Connection, Error};
 
 pub type DatabaseResult<T> = Result<T, Error>;
@@ -32,6 +32,23 @@ impl Operations {
             Ok(_) => Ok(()),
             Err(error) => Err(error),
         }
+    }
+
+    pub fn exists_by_name<T: TableName + FromRow + Named>(
+        &self,
+        name: &str,
+    ) -> DatabaseResult<bool> {
+        let mut statement = self.connection.prepare(
+            &Query::select::<T>()
+                .where_("name", WhereExprOperator::Equal, name)
+                .to_string(),
+        )?;
+        let result = !statement
+            .query_map((), |row| Ok(T::from_row(row)))?
+            .map(|x| x.unwrap())
+            .collect::<Vec<T>>()
+            .is_empty();
+        Ok(result)
     }
 
     pub fn get_all<T: TableName + FromRow>(&self) -> DatabaseResult<Vec<T>> {
@@ -141,5 +158,27 @@ mod test {
         assert_eq!(last_metric_value.metric_id, last_metric.id);
         assert_eq!(last_metric_value.value, 5678);
         assert_eq!(last_metric_value.added, now);
+    }
+    #[test]
+    fn test_exists_by_name() {
+        let ops = Operations::in_memory().unwrap();
+        create_tables_if_do_not_exist(ops.get_connection());
+
+        ops.insert(&Meter::new("meter1")).unwrap();
+        ops.insert(&Meter::new("meter2")).unwrap();
+
+        assert!(ops.exists_by_name::<Meter>("meter1").unwrap());
+        assert!(ops.exists_by_name::<Meter>("meter2").unwrap());
+        assert!(!ops.exists_by_name::<Meter>("meter3").unwrap());
+
+        let meters = ops.get_all::<Meter>().unwrap();
+        ops.insert(&Metric::new("metric1", meters[0].id, 100))
+            .unwrap();
+        ops.insert(&Metric::new("metric2", meters[1].id, 200))
+            .unwrap();
+
+        assert!(ops.exists_by_name::<Metric>("metric1").unwrap());
+        assert!(ops.exists_by_name::<Metric>("metric2").unwrap());
+        assert!(!ops.exists_by_name::<Metric>("metric3").unwrap());
     }
 }
